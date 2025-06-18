34f0d026d485278eb2b288bd8a606136456b4314
VangThao1905 <vangthao1905@gmail.com>
2025-06-18 22:35:20 +0700

    implement image in flutter

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,9 +33,20 @@ class MyAppView extends StatefulWidget {
 }
 
 class _MyAppViewState extends State<MyAppView> {
+  XFile _file = XFile('');
+
   @override
   void initState() {
     super.initState();
+  }
+
+  Future<void> pickImage() async {
+    final result = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (result != null) {
+      setState(() {
+        _file = result;
+      });
+    }
   }
 
   @override
@@ -59,6 +73,22 @@ class _MyAppViewState extends State<MyAppView> {
                     'assets/icons/ic_bird.svg',
                     width: 100,
                     height: 100,
+                  ),
+
+                  Image.network(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMXGTLRjhi5Z8LdEFgaFNyR82Pn8wKmdC4Xw&s',
+                    // width: MediaQuery.of(context).size.width,
+                    // height: 200,
+                  ),
+                  if (_file.path.isNotEmpty) ...{
+                    Image.file(File(_file.path), width: 200, height: 200),
+                  },
+
+                  OutlinedButton(
+                    onPressed: () async {
+                      await pickImage();
+                    },
+                    child: Text('Pick image'),
                   ),
                 ],
               ),
